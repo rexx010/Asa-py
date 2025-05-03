5b35489a7fc397244be8f1611384afece8f9978b
rexx010 <grexx200@gmail.com>
2025-05-03 17:16:37 +0100

    first commit

---
+++ DeitelExcersice8.py
@@ -0,0 +1,31 @@
+zero = 0
+zerosqr = zero * zero
+zerocube = zero ** 3
+
+one = 1
+onesqr = one * one
+onecube = one ** 3
+
+two = 2
+twosqr = two * two
+twocube = two ** 3 
+
+three = 3
+threesqr = three * three 
+threecube = three ** 3 
+
+four = 4
+foursqr = four * four
+fourcube = four ** 3 
+
+five = 5
+fivesqr = five * five
+fivecube = five ** 3 
+
+print('number	square	cube')
+print(zero,	zerosqr,	zerocube)
+print(one,	onecube,	onecube)
+print(two,	twosqr,	twocube)
+print(three,	threesqr,	threecube)
+print(four,	foursqr,	fourcube)
+print(five,	fivesqr,	fivecube)
